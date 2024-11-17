12ac63c9f27b1925e0747610a6ff6342c51a5add
Cris <cristianarellanoagudo@gmail.com>
2024-11-17 19:59:14 +0100

    Messages changed

--- GameStats/messages.py
+++ GameStats/messages.py
@@ -1,9 +1,9 @@
 # messages.py
 
-messages = {
+messagesStrings = {
     "select_language": "Select language for stats:",
-    "select_game_en": "Select game for stats:\n\n1️⃣ Fortnite\n\n2️⃣ Valorant\n",
-    "select_game_es": "Selecciona el juego para estadísticas:\n\n1️⃣ Fortnite\n\n2️⃣ Valorant\n",
+    "select_game_en": "Select game for stats:",
+    "select_game_es": "Selecciona el juego para estadísticas:",
     "fortnite_info_en": "This is the Fortnite bot! It provides information and commands related to Fortnite.",
     "valorant_info_en": "This is the Valorant bot! It provides information and commands related to Valorant.",
     "fortnite_info_es": "¡Este es el bot de Fortnite! Proporciona información y comandos relacionados con Fortnite.",
